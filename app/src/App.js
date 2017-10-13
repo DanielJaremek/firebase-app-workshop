4102f84fea10d894be2ec4e664ca5a72edbfa461
DanielJaremek <djaremek11@gmail.com>
2017-10-13 14:09:43 +0200

    fun with firebase

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,82 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {database, auth} from './firebase'
 
 class App extends Component {
+  state = {
+    email: '',
+    password: '',
+    isLoggedIn: false,
+    userData: ''
+  }
+
+  emailHandler = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+
+  passwordHandler = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  login = () => {
+    auth.signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => {
+          console.log('LOGGED IN')
+
+        })
+        .catch(console.log)
+  }
+
+  logout = () => {
+    auth.signOut()
+        .then(() => {
+          console.log('LOGGED OUT')
+        })
+        .catch(console.log)
+  }
+
+  componentWillMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          isLoggedIn: true
+        })
+        database.ref(`usersData/${auth.currentUser.uid}/data`).on('value', (snapshot) => {
+          this.setState({
+            userData: JSON.stringify(snapshot.val())
+          })
+
+        })
+      } else {
+        this.setState({
+          isLoggedIn: false
+        })
+      }
+    })
+  }
+
   render() {
+    const app = <div>{this.state.userData || null}
+      <button onClick={this.logout}>LogOUT</button>
+    </div>
+    const loginForm = <div>
+      <input type="text" onChange={this.emailHandler} value={this.state.email}/>
+      <input type="password" onChange={this.passwordHandler} value={this.state.password}/>
+
+      <button onClick={this.login}>Login</button>
+
+    </div>
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        this.state.isLoggedIn ? app : loginForm
+
+    )
   }
 }
 
---
+++ app/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+let config = {
+  apiKey: "AIzaSyDRiZbJJQ9H9EO2dwSPEu2_HOZNK1AcPXo",
+  authDomain: "my-business-card-b7abb.firebaseapp.com",
+  databaseURL: "https://my-business-card-b7abb.firebaseio.com",
+  projectId: "my-business-card-b7abb",
+  storageBucket: "my-business-card-b7abb.appspot.com",
+  messagingSenderId: "37183021582"
+};
+let firebaseApp = firebase.initializeApp(config);
+
+export const database = firebaseApp.database();
+export const auth  = firebaseApp.auth();
+export default firebaseApp;
